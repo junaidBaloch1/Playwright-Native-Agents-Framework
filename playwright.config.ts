fc872661521883fcf14a83fe88ff49e07788f8c1
Fullstack Developer 91 <fullstackdeveloper91@192.168.1.7>
2026-06-15 13:21:10 +0500

    config and yml updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   use: {
     baseURL: process.env.BASE_URL || 'https://www.saucedemo.com/',
 
-    headless: false,   
+    headless: true,   
 
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
